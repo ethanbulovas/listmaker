47c343598d6ca6f167ac9654ae00b0030893392b
William-Bulovas <will.bulo@gmail.com>
2017-03-11 18:42:37 -0500

    Make a button Add some behaviour to the button

--- app/src/main/java/com/app/william/listmaker/MainActivity.java
+++ app/src/main/java/com/app/william/listmaker/MainActivity.java
@@ -2,6 +2,11 @@ package com.app.william.listmaker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final TextView helloWorld = (TextView) findViewById(R.id.helloWorld);
+        Button whatUp = (Button) findViewById(R.id.whatupbutton);
+        whatUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                helloWorld.setText("Not much");
+            }
+        });
     }
 }
